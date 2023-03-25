b6d43ee20d315be3e0bcae00fdd8a46cd1b2d77e
charan68 <17cn203@gmail.com>
2023-03-25 15:57:13 +0530

    mycommit

---
+++ src/Pokecard.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './Pokecard.css';
+
+const pokeapi= 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padnum = (number)=> (number<=999) ? `00${number}`.slice(-3) : number;
+
+class Pokecard extends Component{
+    render(){
+        let imgSrc = `${pokeapi}${padnum(this.props.id)}.png`;
+        return(
+            <div className="Pokecard">
+              <h1 className="Pokecard-title">{this.props.name}</h1>
+              <div className="Pokecard-image"><img src={imgSrc} alt={this.props.name}/></div>
+              <div className="Pokecard-data">Type: {this.props.type}</div>
+              <div className="Pokecard-data">EXP: {this.props.exp}</div>
+            </div>
+        )
+    }
+}
+export default Pokecard;
